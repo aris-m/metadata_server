2d60184aa0e45f710ce6efa60af72275cfdc8af0
aris-m <aris.munandar3141@gmail.com>
2024-07-10 23:30:16 +0200

    create metadata model

--- metadata_server_app/models.py
+++ metadata_server_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class MetaData(models.Model):
+    data_format_choices = [
+        ('csv', 'csv'),
+        ('xlsx', 'xlsx'),
+        ('tsv', 'tsv'),
+        ('json', 'json'),
+        ('xml', 'xml'),
+        ('txt', 'txt'),
+        ('md', 'md'),
+        ('tex', 'tex'),
+        ('jpeg', 'jpeg'),
+        ('png', 'png'),
+        ('zip', 'zip'),
+        ('tar', 'tar'),
+        ('other', 'other'),
+    ]
+    
+    
+    data_provider = models.CharField(max_length=100, strip=True)
+    data_format = models.CharField(max_length=20, strip=True)
+    other_data_format = models.CharField(max_length=20, blank=True, null=True)
+    degree_of_aggregation = models.CharField(max_length=100, strip=True)
