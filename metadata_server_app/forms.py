8ec7bba04363ee4d758e8c28527449b0e5537a1b
aris-m <aris.munandar3141@gmail.com>
2024-07-11 13:26:43 +0200

    add validation to other_data_format field

--- metadata_server_app/forms.py
+++ metadata_server_app/forms.py
@@ -5,3 +5,9 @@ class MetadataForm(forms.ModelForm):
     class Meta:
         model = Metadata
         fields = ['data_provider', 'data_format', 'other_data_format', 'degree_of_aggregation']
+        
+        def clean(self):
+            data_format = self.cleaned_data.get('data_format')
+            other_data_format = self.cleaned_data.get('other_data_format')
+            if data_format == 'other' and not other_data_format:
+                raise forms.ValidationError({'other_data_format': 'This field is required when "other" is selected.'})
--- metadata_server_app/models.py
+++ metadata_server_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Metadata(models.Model):
     data_format_choices = [
@@ -22,3 +23,7 @@ class Metadata(models.Model):
     data_format = models.CharField(max_length=20, choices=data_format_choices)
     other_data_format = models.CharField(max_length=20, blank=True, null=True)
     degree_of_aggregation = models.CharField(max_length=100)
+    
+    def clean(self):
+        if self.data_format == 'other' and not self.other_data_format:
+            raise ValidationError({'other_data_format': 'This field is required when "Other" is selected.'})
