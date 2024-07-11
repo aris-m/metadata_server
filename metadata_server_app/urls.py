31ed705730888b9d0298920b7e7feb830a269f41
aris-m <aris.munandar3141@gmail.com>
2024-07-11 16:03:34 +0200

    add metadata list web page

--- metadata_server_app/urls.py
+++ metadata_server_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from . import plugins
 
 urlpatterns = [
-    path('', views.insert_metadata, name='insert-metadata')
+    path('', views.list_metadata, name='list-metadata'),
+    path('download_readme/<int:metadata_id>/', plugins.download_readme, name='download_readme'),
+    path('insert-metadata/', views.insert_metadata, name='insert-metadata')
 ]
--- metadata_server_app/views.py
+++ metadata_server_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import MetadataForm
+from .models import Metadata
 
 def insert_metadata(request):
     if request.method == 'POST':
@@ -10,3 +11,7 @@ def insert_metadata(request):
     else:
         form = MetadataForm()
     return render(request, 'metadata_form.html', {'form': form})
+
+def list_metadata(request):
+    metadata_list = Metadata.objects.all()
+    return render(request, 'metadata_list.html', {'metadata_list' : metadata_list})
